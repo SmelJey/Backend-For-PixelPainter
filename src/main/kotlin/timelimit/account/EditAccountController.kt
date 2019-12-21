f162e678752ab166ae0e6b42a92cc3f8853f5236
Юрий Михалев <mixalevuriy@gmail.com>
2019-12-21 16:56:28 +1000

    Add @Cross-Origin for EditAccountController

--- src/main/kotlin/timelimit/account/EditAccountController.kt
+++ src/main/kotlin/timelimit/account/EditAccountController.kt
@@ -4,6 +4,7 @@ import org.jetbrains.exposed.sql.select
 import org.jetbrains.exposed.sql.transactions.transaction
 import org.jetbrains.exposed.sql.update
 import org.joda.time.DateTime
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
@@ -12,6 +13,7 @@ import java.net.URI
 import java.security.MessageDigest
 import kotlin.math.sin
 
+@CrossOrigin(origins = ["http://localhost:8081"], maxAge = 3600)
 @RestController
 class EditAccountController {
     class Edit constructor(val status: String)
