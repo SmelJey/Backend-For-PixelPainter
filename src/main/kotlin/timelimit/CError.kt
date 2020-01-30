11bf582453e44fd4b57d2b0df939e19a41a92655
Vladislav Kalinin <unkorunk@gmail.com>
2020-01-31 00:51:04 +1000

    Add error handling

---
+++ src/main/kotlin/timelimit/CError.kt
@@ -0,0 +1,22 @@
+package timelimit
+
+import org.springframework.boot.web.servlet.error.ErrorController
+import org.springframework.web.bind.annotation.CrossOrigin
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import javax.servlet.http.HttpServletResponse
+
+@CrossOrigin(origins = ["http://localhost:8081"], maxAge = 3600)
+@RestController
+class CError : ErrorController {
+    class Error constructor(val status: String, val type_error: Int)
+
+    @RequestMapping("/error")
+    fun error(resp : HttpServletResponse) : Error {
+        return Error("ERROR", resp.status)
+    }
+
+    override fun getErrorPath(): String {
+        return "/error"
+    }
+}
